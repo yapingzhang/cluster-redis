47c9ccb253c4863ce9e3c318264eab5699c2cc74
朱伟亮 <zhuwl120820@gxwsxx.com>
2014-12-12 20:27:24 +0800

    优化redis.conf的引入

--- home/redis-cluster/init.sh
+++ home/redis-cluster/init.sh
@@ -7,15 +7,19 @@ then
 	redis_cluster_node_count=10
 fi
 
-for ((i=0;i<${tomcat_cluster_node_count};i++)); do
+rm -rf ${redis_cluster_base}/${redis_cluster_port}*
+
+for ((i=0;i<${redis_cluster_node_count};i++)); do
     node_port=${redis_cluster_port}${i}
     node_dir=${redis_cluster_base}/${node_port}
     mkdir ${node_dir}
     cp ${redis_cluster_base}/conf/redis-node.conf ${node_dir}
     node_conf=${node_dir}/redis-node.conf
-    sed -i -e '/include/a '"${redis_cluster_work_base}"'/redis.conf' ${node_conf}
-    sed -i -e '/port/a '"${node_port}"'' ${node_conf}
-    sed -i -e '/pidfile/a '"${node_dir}"'/redis.pid' ${node_conf}
+#    sed -i -e '/include/a '"${redis_cluster_work_base}"'/redis.conf' ${node_conf}
+#    sed -i -e '/port/a '"${node_port}"'' ${node_conf}
+#    sed -i -e '/pidfile/a '"${node_dir}"'/redis.pid' ${node_conf}
+    sed -i -e '$a include '"${redis_cluster_work_base}"'/redis.conf \n port '"${node_port}"' \n pidfile '"${node_dir}"'/redis.pid' ${node_conf}
+
 done
 
-chown -R redis_cluster_user:redis_cluster_user ${redis_cluster_base}/*
+chown -R ${redis_cluster_user}:${redis_cluster_user} ${redis_cluster_base}/*
