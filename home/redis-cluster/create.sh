a64ca4912fad81228cba37d069167a6761a7a940
朱伟亮 <zhuwl120820@gxwsxx.com>
2015-01-01 15:12:46 +0800

    update：修改集群创建方式

--- home/redis-cluster/create.sh
+++ home/redis-cluster/create.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo "创建redis集群..."
 redis_cluster_base=$(cd `dirname $0`; pwd)
 . ${redis_cluster_base}/conf/redis-cluster.conf
 
@@ -10,16 +11,4 @@ for ((i=0;i<${redis_cluster_node_count};i++)); do
     done
 done
 
-echo ${redis_cluster_list}
-
-#${redis_cluster_work_base}/src/redis-trib.rb create --replicas 1 ${redis_cluster_list}
-
-#${redis_cluster_work_base}/src/redis-trib.rb create --replicas 1 \
-#192.168.105.21:13300 \
-#192.168.105.22:13300 \
-#192.168.105.21:13301 \
-#192.168.105.22:13301 \
-#192.168.105.21:13302 \
-#192.168.105.22:13302 \
-#192.168.105.21:13303 \
-#192.168.105.22:13303 
+${redis_cluster_work_base}/src/redis-trib.rb create --replicas 1 ${redis_cluster_list}
